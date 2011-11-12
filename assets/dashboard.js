2c749578610434b01b6c9b37763ad615ea34061f
Jason Mulligan <jason.mulligan@avoidwork.com>
2011-11-12 15:28:27 -0500

    Trying intro retrieval again

--- assets/dashboard.js
+++ assets/dashboard.js
@@ -42,7 +42,8 @@ $.on("ready", function() {
 
 	$("#stage").on("beforeGet", function() { this.loading(); }, "loading")
 	           .on("afterGet",  function() { this.opacity(0).fade(1000); }, "fade")
-	           .on("afterFade", function() { if (typeof $("#twitter") !== "undefined") twitter(); }, "twitter");
+	           .on("afterFade", function() { if (typeof $("#twitter") !== "undefined") twitter(); }, "twitter")
+	           .get("views/intro.htm");
 
 	// Setting up Wordpress retrieval & display
 	$.store(blog);
