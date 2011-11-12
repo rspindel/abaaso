a0db8faa07769a11acfa9372aceb86426a2327de
Jason Mulligan <jason.mulligan@avoidwork.com>
2011-11-12 15:00:16 -0500

    Removing intro GET for testing

--- assets/dashboard.js
+++ assets/dashboard.js
@@ -42,8 +42,7 @@ $.on("ready", function() {
 
 	$("#stage").on("beforeGet", function() { this.loading(); }, "loading")
 	           .on("afterGet",  function() { this.opacity(0).fade(1000); }, "fade")
-	           .on("afterFade", function() { if (typeof $("#twitter") !== "undefined") twitter(); }, "twitter")
-	           .get("views/intro.htm");
+	           .on("afterFade", function() { if (typeof $("#twitter") !== "undefined") twitter(); }, "twitter");
 
 	// Setting up Wordpress retrieval & display
 	$.store(blog);
