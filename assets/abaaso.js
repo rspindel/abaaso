c44883a843288444eac47c8d0d4fe8201b7e6501
Jason Mulligan <jason.mulligan@avoidwork.com>
2012-02-03 18:58:08 -0500

    Upgraded abaaso

--- assets/abaaso.js
+++ assets/abaaso.js
@@ -4122,7 +4122,7 @@
 		expires         : 120000,
 		extend          : utility.extend,
 		fire            : function (obj, event, arg) {
-			var all = typeof event !== "undefined",
+			var all = typeof arg !== "undefined",
 			    o, e, a;
 
 			o = all ? obj   : this;
