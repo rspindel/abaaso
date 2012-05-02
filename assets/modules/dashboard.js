93ab8a74ba5a7c853e92ad64c6cca8787696186e
Jason Mulligan <jason.mulligan@avoidwork.com>
2012-05-01 21:49:30 -0400

    Fixing blog

--- assets/modules/dashboard.js
+++ assets/modules/dashboard.js
@@ -14,34 +14,62 @@
 			    id      : "twitter",
 			    display : function (index) {
 			    	index = index || 0;
-			    	var r = this.data.get(index);
+			    	var r = this.data.get(index),
+			    	    t = $("#twitter");
 
-			    	$("#twitter").create("p").text(typeof r !== "undefined" ? r.data.text : "Nothing in the fire hose");
+			    	if (typeof t !== "undefined") t.clear().create("p").text(typeof r !== "undefined" ? r.data.text : "Nothing in the fire hose");
 			    }
 			},
-			ready, render;
+			displayBlog, ready, render;
 
-		ready = function ($, d) {
-			$.store(d.blog);
-			d.blog.data.key         = "id";
-			d.blog.data.callback    = "jsonp";
-			d.blog.data.source      = "response";
-			d.blog.data.uri         = "http://api.tumblr.com/v2/blog/attackio.tumblr.com/posts?api_key=cm7cZbxWpFDtv8XFD5XFuWsn5MnzupVpUtaCjYIJAurfPj5B1V&tag=abaaso&limit=1000000&jsonp=?";
+		displayBlog = function (data) {
+			var items = data.get(0, 9),
+			    obj   = $("section[data-hash='blog']").first(),
+			    d, o;
 
-			$.store(d.collabs);
-			d.collabs.data.key      = "id";
-			d.collabs.data.callback = "callback";
-			d.collabs.data.source   = "data";
-			d.collabs.data.uri      = "https://api.github.com/repos/avoidwork/abaaso/collaborators?callback=?";
+			obj.clear();
 
-			$.store(d.twitter);
-			d.twitter.data.key      = "id";
-			d.twitter.data.callback = "callback";
-			d.twitter.on("afterDataSync", function() { this.display(); }, "sync");
-			d.twitter.data.uri      = "http://search.twitter.com/search.json?callback=?&from=abaaso";
+			if (typeof items.first() !== "undefined") {
+				items.each(function (item) {
+					d = item.data.date.replace(/\s.*/, "").explode("-"); // Parsing String because some browsers will not cast to Date
+					o = obj.create("article");
+					o.create("h3").create("a", {href: item.data.post_url, innerHTML: item.data.title});
+					o.create("date").text($.label.month[parseInt(d[1] -1 ).toString()]+" "+d[2]+", "+d[0]);
+					o.create("entry").text(item.data.body);
+				});
+				obj.create("p").create("a", {innerHTML: "Read more on attack.io", href: "http://attack.io"});
+			}
+			else obj.create("p").html("No posts to display.");
 		};
 
-		render = function ($, dashboard) {
+		ready = function ($, d) {
+			var blog    = d.blog,
+			    collabs = d.collabs,
+			    twitter = d.twitter,
+			    data;
+
+			$.store(blog);
+			data = blog.data;
+			blog.on("afterDataSync", function () { displayBlog(data); });
+			data.key         = "id";
+			data.callback    = "jsonp";
+			data.source      = "response";
+			data.uri         = "http://api.tumblr.com/v2/blog/attackio.tumblr.com/posts?api_key=cm7cZbxWpFDtv8XFD5XFuWsn5MnzupVpUtaCjYIJAurfPj5B1V&tag=abaaso&limit=1000000&jsonp=?";
+
+			$.store(collabs);
+			collabs.data.key      = "id";
+			collabs.data.callback = "callback";
+			collabs.data.source   = "data";
+			collabs.data.uri      = "https://api.github.com/repos/avoidwork/abaaso/collaborators?callback=?";
+
+			$.store(twitter);
+			twitter.data.key      = "id";
+			twitter.data.callback = "callback";
+			twitter.on("afterDataSync", function() { this.display(); }, "sync");
+			twitter.data.uri      = "http://search.twitter.com/search.json?callback=?&from=abaaso";
+		};
+
+		render = function ($, d) {
 			var stage  = $("#stage"),
 			    obj, root;
 
@@ -49,60 +77,32 @@
 			stage.tabs(["Main", "API", "Blog", "Download", "Examples"]);
 
 			// Setting routing
-			$.route.set("download", function () {
-				$("section[data-hash='download']")[0].get("views/download.htm");
-			});
-
+			$.route.set("download", function () { $("section[data-hash='download']").first().get("views/download.htm"); });
 			$.route.set("blog", function () {
-				var fn  = function () {
-					if (dashboard.blog.data.total > 0) {
-						var items = dashboard.blog.data.get(0, 9), d, o;
-
-						obj.clear();
-
-						items.each(function (item) {
-							d = item.data.date.replace(/\s.*/, "").explode("-"); // Parsing String because some browsers will not cast to Date
-							o = obj.create("article");
-							o.create("h3").create("a", {href: item.data.post_url, innerHTML: item.data.title});
-							o.create("date").text($.label.month[parseInt(d[1] -1 ).toString()]+" "+d[2]+", "+d[0]);
-							o.create("entry").text(item.data.body);
-						});
-
-						obj.create("p").create("a", {innerHTML: "Read more on attack.io", href: "http://attack.io"});
-						return false;
-					}
-					else obj.create("p").html("No posts to display.");
-				};
-
-				obj = $("section[data-hash='blog']")[0];
-				obj.loading();
-
-				$.repeat(fn, 10, "blog");
+				$("section[data-hash='blog']").first().loading();
+				displayBlog(d.blog.data);
 			});
-
 			$.route.set("api", function () {
-				$("section[data-hash='api']").html("Redirecting to Github");
+				$("section[data-hash='api']").first().html("Redirecting to Github");
 				location = "https://github.com/avoidwork/abaaso/wiki";
 			});
-
 			$.route.set("error", function () {
-				$("section[data-hash='main']")[0].get("views/error.htm");
-			});
+				var guid = $.guid(true);
 
-			$.route.set("examples", function () {
-				$("section[data-hash='examples']")[0].get("views/examples.htm");
+				$("section[data-hash='main']").first().on("afterGet", function () {
+					this.un("afterGet", guid);
+					$.tabs.active("main");
+				}, guid).get("views/error.htm");
 			});
-
-			$.route.set("main", function () {
-				$("section[data-hash='main']")[0].get("views/intro.htm");
-			});
+			$.route.set("examples", function () { $("section[data-hash='examples']").first().get("views/examples.htm"); });
+			$.route.set("main", function () { $("section[data-hash='main']").first().get("views/intro.htm"); });
 
 			// Prepping the UI
 			$.loading.url = "assets/loading.gif";
 			$("version").text($.version);
 			$("year").text(new Date().getFullYear());
 			$("section").on("beforeGet", function () { this.loading(); }, "loading");
-			$("section[data-hash='main']").on("afterGet", function () { twitter.display(); }, "twitter");
+			$("section[data-hash='main']").first().on("afterGet", function () { twitter.display(); }, "twitter");
 			$("ul.tab a").addClass("shadow round button padded");
 			$("body").css("opacity", 1);
 
