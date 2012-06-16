f0cfd4c2bf82f0897108c3dc5776106d827d64c6
Jason Mulligan <jason.mulligan@avoidwork.com>
2012-06-16 15:56:40 -0400

    Updating module & test

--- modules/dashboard.js
+++ modules/dashboard.js
@@ -26,8 +26,8 @@
 			var obj      = $("section[data-hash='blog']").first(),
 			    template = "<article><h3><a href={{post_url}}>{{title}}</a></h3><date>{{date}}</date><entry>{{body}}</entry></article>";
 
-			blog.on("afterDataListRefresh", function () {
-				this.datalist.element.find("li > date").each(function (i) {
+			blog.on("afterDataListRefresh", function (element) {
+				element.find("li > date").each(function (i) {
 					i.text(moment(i.text()).format("dddd, MMMM Do YYYY, h:mm a"));
 				});
 			}, "moment");
