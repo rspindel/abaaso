1f18f9d944a7170d4c9967022108fdf9ca34216c
Jason Mulligan <jason.mulligan@avoidwork.com>
2012-06-16 17:17:33 -0400

    Loading debugging versions

--- modules/abaaso.datalist.js
+++ modules/abaaso.datalist.js
@@ -32,7 +32,188 @@
  *
  * @author Jason Mulligan <jason.mulligan@avoidwork.com>
  * @link http://avoidwork.com
+ * @module abaaso.datalist
  * @requires abaaso 2.1.7
  * @version 1.0.5
  */
-(function(a){"use strict";var b,c;b=function(b,c,d,e){var f=a[abaaso.aliased],g=this,h;if(b instanceof Element&&typeof c=="object"&&!!/string|object/.test(typeof d))return h=b.create("ul",{"class":"list"}),this.element=h,this.end=null,this.callback=null,this.filter=null,this.placeholder="",this.order="",this.ready=!1,this.template=d,this.sensitivity="ci",this.start=null,this.store=c,this.store.parentNode.on("afterDataSync",function(){this.refresh()},"refresh-"+h.id,this),this.store.parentNode.on("afterDataDelete",function(a){f("#"+h.id+" li[data-key='"+a.key+"']").destroy()},"delete-"+h.id,this),e instanceof Object&&f.iterate(e,function(a,b){g[b]=a}),this.refresh(),this;throw Error(f.label.error.invalidArguments)},b.prototype.refresh=function(){var b=a[abaaso.aliased],c=this.element,d=typeof this.template=="object",e=!d&&String(this.template).replace(/{{|}}/g,"")===this.store.key,f=[],g=[],h=this,i=b.encode(this.template).indexOf("data-key")<0,j=typeof this.callback=="function",k=/{{.*}}/g,l=new RegExp,m=[],n;return this.ready||(this.ready=!0,this.store.parentNode.on("afterDataSet",function(a){this.refresh()},"set-"+c.id,this)),this.store.parentNode.fire("beforeDataListRefresh"),d?n=function(a){var c=b.encode(h.template);return c=c.replace("{{"+h.store.key+"}}",a.key),b.iterate(a.data,function(a,d){l.compile("{{"+d+"}}","g"),c=c.replace(l,b.encode(a).replace(/(^")|("$)/g,""))}),c=b.decode(c.replace(k,h.placeholder)),{li:c}}:n=function(a){var c=String(h.template);return c=c.replace("{{"+h.store.key+"}}",a.key),b.iterate(a.data,function(a,b){l.compile("{{"+b+"}}","g"),c=c.replace(l,a)}),{li:c.replace(k,h.placeholder)}},f=this.order.isEmpty()?this.store.get():this.store.sort(this.order,!1,this.sensitivity),typeof this.start=="number"&&typeof this.end=="number"&&(f=f.range(this.start,this.end)),f.each(function(a){h.filter!==null&&h.filter instanceof Object?b.iterate(h.filter,function(b,c){if(m.index(a.key)>-1)return;var d=0,e=new RegExp,f;b=b.explode(),f=b.length;for(d=0;d<f;d++){e.compile(b[d],"i");if(c===h.store.key&&e.test(a.key)||typeof a.data[c]!="undefined"&&e.test(a.data[c])){m.push(a.key),g.push({key:a.key,template:n(a)});return}}}):g.push({key:a.key,template:n(a)})}),c.clear(),g.each(function(a){var d=b.genId();b.on("afterCreate",function(c){b.un("afterCreate",d),i&&c.data("key",a.key),j&&h.callback(c)},d),c.tpl(a.template)}),this.store.parentNode.fire("afterDataListRefresh",this.element),this},b.prototype.sort=function(b,c){var d=a[abaaso.aliased];if(typeof b!="string")throw Error(d.label.error.invalidArguments);return this.store.parentNode.fire("beforeDataListSort"),this.order=b,this.sensitivity=c||"ci",this.refresh(),this.store.parentNode.fire("afterDataListSort"),this},c=function(a){return a.module("datalist",b),a.datalist},typeof define=="function"?define(["abaaso"],function(a){return c(a)}):abaaso.on("init",function(){c(abaaso)},"abaaso.datalist")})(this)
+(function (global) {
+	"use strict";
+
+	var datalist, fn;
+
+	/**
+	 * DataList Factory
+	 *
+	 * Options: callback    Function to execute after creating a templated record display, parameter is the new Element
+	 *          filter      Object describing key:value pairs to filter the list on ({property: "someValue"}), accepts comma delimited values
+	 *          placeholder String to use in lieu of an undefined record (data) property
+	 *          start       Start position for pagination
+	 *          end         End position for pagination
+	 *           
+	 * @param  {Object} target   Element to receive the DataList
+	 * @param  {Object} store    Data store to feed the DataList
+	 * @param  {Mixed}  template Record field, template ($.tpl), or String, e.g. "<p>this is a {{field}} sample.</p>", fields are marked with {{ }}
+	 * @param  {Object} options  Optional parameters to set on the DataList
+	 * @return {Object}          DataList instance
+	 */
+	datalist = function (target, store, template, options) {
+		var $    = global[abaaso.aliased],
+		    self = this,
+		    element;
+
+		if (!(target instanceof Element) || typeof store !== "object" || !/string|object/.test(typeof template)) throw Error($.label.error.invalidArguments);
+
+		element          = target.create("ul", {"class": "list"});
+		this.element     = element;
+		this.end         = null;
+		this.callback    = null;
+		this.filter      = null;
+		this.placeholder = "";
+		this.order       = "";
+		this.ready       = false;
+		this.template    = template;
+		this.sensitivity = "ci";
+		this.start       = null;
+		this.store       = store;
+		this.store.parentNode.on("afterDataSync",   function ()  { this.refresh(); }, "refresh-" + element.id, this);
+		this.store.parentNode.on("afterDataDelete", function (r) { $("#" + element.id + " li[data-key='" + r.key + "']").destroy(); }, "delete-" + element.id, this);
+		if (options instanceof Object) $.iterate(options, function (v, k) { self[k] = v; });
+		this.refresh();
+		return this;
+	};
+
+	/**
+	 * Refreshes element
+	 * 
+	 * Events: beforeDataListRefresh, afterDataListRefresh
+	 * 
+	 * @return {Object} DataList instance
+	 */
+	datalist.prototype.refresh = function () {
+		var $        = global[abaaso.aliased],
+		    element  = this.element,
+		    template = (typeof this.template === "object"),
+		    key      = (!template && String(this.template).replace(/{{|}}/g, "") === this.store.key),
+		    consumed = [],
+		    items    = [],
+		    self     = this,
+		    attr     = ($.encode(this.template).indexOf("data-key") < 0),
+		    callback = (typeof this.callback === "function"),
+		    cleanup  = /{{.*}}/g,
+		    regex    = new RegExp(),
+		    registry = [], // keeps track of records in the list (for filtering)
+		    fn;
+
+		if (!this.ready) {
+			this.ready = true;
+			this.store.parentNode.on("afterDataSet", function (r)  { this.refresh(); }, "set-" + element.id, this);
+		}
+
+		this.store.parentNode.fire("beforeDataListRefresh");
+
+		// Creating templates for the html rep
+		if (!template) fn = function (i) {
+			var html = String(self.template);
+
+			html = html.replace("{{" + self.store.key + "}}", i.key)
+			$.iterate(i.data, function (v, k) {
+				regex.compile("{{" + k + "}}", "g");
+				html = html.replace(regex, v);
+			});
+			return {li: html.replace(cleanup, self.placeholder)};
+		}
+		else fn = function (i) {
+			var obj = $.encode(self.template);
+
+			obj = obj.replace("{{" + self.store.key + "}}", i.key)
+			$.iterate(i.data, function (v, k) {
+				regex.compile("{{" + k + "}}", "g");
+				obj = obj.replace(regex, $.encode(v).replace(/(^")|("$)/g, "")); // stripping first and last " to concat to valid JSON
+			});
+			obj = $.decode(obj.replace(cleanup, self.placeholder));
+			return {li: obj};
+		};
+
+		// Consuming records based on sort
+		consumed = this.order.isEmpty() ? this.store.get() : this.store.sort(this.order, false, this.sensitivity);
+		if (typeof this.start === "number" && typeof this.end === "number") consumed = consumed.range(this.start, this.end);
+
+		// Processing (filtering) records & generating templates
+		consumed.each(function (i) {
+			if (self.filter === null || !(self.filter instanceof Object)) items.push({key: i.key, template: fn(i)});
+			else {
+				$.iterate(self.filter, function (v, k) {
+					if (registry.index(i.key) > -1) return;
+
+					var x     = 0,
+					    regex = new RegExp(),
+					    nth;
+
+					v   = v.explode();
+					nth = v.length;
+
+					for (x = 0; x < nth; x++) {
+						regex.compile(v[x], "i");
+						if ((k === self.store.key && regex.test(i.key)) || (typeof i.data[k] !== "undefined" && regex.test(i.data[k]))) {
+							registry.push(i.key);
+							items.push({key: i.key, template: fn(i)});
+							return;
+						}
+					}
+				});
+			}
+		});
+
+		// Preparing the target element
+		element.clear().on("afterTemplate", function (obj) { if (callback) self.callback(obj); }, "callback");
+		items.each(function (i) {
+			if (attr) {
+				var guid = $.genId();
+
+				$.on("afterCreate", function (obj) {
+					$.un("afterCreate", guid);
+					obj.data("key", i.key);
+				}, guid);
+			}
+			element.tpl(i.template);
+		});
+		element.un("afterTemplate", "callback");
+		this.store.parentNode.fire("afterDataListRefresh", this.element);
+		return this;
+	};
+
+	/**
+	 * Sorts data list & refreshes element
+	 * 
+	 * Events: beforeDataListRefresh, afterDataListRefresh
+	 * 
+	 * @param  {String} order       SQL "order by" statement
+	 * @param  {String} sensitivity [Optional] Defaults to "ci" ("ci" = insensitive, "cs" = sensitive, "ms" = mixed sensitive)
+	 * @return {Object}       DataList instance
+	 */
+	datalist.prototype.sort = function (order, sensitivity) {
+		var $ = global[abaaso.aliased];
+
+		if (typeof order !== "string") throw Error($.label.error.invalidArguments);
+
+		this.store.parentNode.fire("beforeDataListSort");
+		this.order       = order;
+		this.sensitivity = sensitivity || "ci";
+		this.refresh();
+		this.store.parentNode.fire("afterDataListSort");
+		return this;
+	};
+
+	/**
+	 * Initialization
+	 * 
+	 * @param  {Object} abaaso abaaso singleton
+	 * @return {Function}      datalist module
+	 */
+	fn = function (abaaso) {
+		abaaso.module("datalist", datalist);
+		return abaaso.datalist;
+	};
+
+	typeof define === "function" ? define(["abaaso"], function (abaaso) { return fn(abaaso); }) : abaaso.on("init", function () { fn(abaaso); }, "abaaso.datalist");
+})(this);
